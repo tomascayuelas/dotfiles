7d406207a8e263503fac93184bfdabb8b672cd09
Tomas Cayuelas <hello@tomascayuelas.com>
2020-07-25 00:21:33 +0200

    Remove shells configs ans reduce install.sh

--- install.sh
+++ install.sh
@@ -2,26 +2,6 @@
 
 export DOTFILES_PATH="${HOME}/Development/Own/dotfiles"
 
-function checkDotfilePathEnv()
-{
-    messageDotfileNotFound="DotfilePath not found"
-    if [[ ! -v "DOTFILES_PATH" ]]; then
-      echo $messageDotfileNotFound
-      exit
-    fi
-}
-
-function bash()
-{
-  ln -s -i ${DOTFILES_PATH}/shells/bash/.bashrc $HOME/.bashrc
-  ln -s -i ${DOTFILES_PATH}/shells/bash/.bash_profile $HOME/.bash_profile
-}
-
-function vimEditor()
-{
-  ln -s -i ${DOTFILES_PATH}/editors/vim/.vimrc $HOME/.vimrc
-}
-
 function git()
 {
   ln -s -i ${DOTFILES_PATH}/git/.gitconfig $HOME/.gitconfig
@@ -35,12 +15,11 @@ function i3wm()
   ln -s -i ${DOTFILES_PATH}/i3 $HOME/.config/i3
 }
 
-function xinitrc()
+function nvim()
 {
-  ln -s -i ${DOTFILES_PATH}/X/.xinitrc $HOME/.xinitrc
+  ln -s -i $DOTFILES_PATH/nvim $HOME/.config/nvim
 }
 
-<<<<<<< HEAD
 function xresources()
 {
   ln -s -i ${DOTFILES_PATH}/X/.Xresources.d $HOME/
@@ -52,11 +31,5 @@ function karabiner()
   ln -s -i ${DOTFILES_PATH}/mac/karabiner $HOME/.config/karabiner
 }
 
-=======
->>>>>>> fe8d7fd31d7f5c276849abf503d32b52a24db2ac
-bash
-vimEditor
 git
-i3wm
-xinitrc
-karabiner
+nvim
--- shells/env/exports.sh
+++
@@ -1,2 +0,0 @@
-export SBT_OPTS=-'Xms512M -Xmx1024M -Xss6M -XX:MaxMetaspaceSize=512m -XX:+CMSClassUnloadingEnabled'
-export PATH=$PATH:~/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES_PATH/git/bin:$DOTFILES_PATH/scripts:~/.composer/vendor/bin:$JAVA_HOME/bin:/usr/local/opt/python/libexec/bin:
