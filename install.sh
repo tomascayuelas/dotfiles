c43dc3301bde420e9dbe7d525d4eb196f48c42f1
Tomas Cayuelas <hello@tomascayuelas.com>
2020-09-09 01:06:54 +0200

    nvim configuration zero, and add some features to install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export DOTFILES_PATH="${HOME}/Development/dotfiles"
 
+function checker()
+{
+    local ORIG=$1
+    local DEST=$2
+
+    if [[ -d "${DEST}" || -f "${DEST}" || -L "${DEST}" ]]; then
+	echo "> ${DEST} already exists, what do you want?" 
+	echo " b. Backup"
+	echo " r. Replace"
+
+	read -n 1 -p "> What do you want: " selected
+	echo -e "\n"
+
+	case "$selected" in
+	    "b")
+		tar -cvpzf ${DEST}-backup.tar.gz ${DEST}
+		;;
+	    "r")
+		rm -fr ${DEST}
+		return 0
+	        ;;	
+	    *)
+		echo "Invalid input, try again... :cross_finger:"
+		checker $ORIG $DEST
+		;;
+	esac
+    fi
+}
+
+function lnCommand()
+{
+    local ORIG=${DOTFILES_PATH}/$1
+    local DEST=${HOME}/$2
+
+    checker $ORIG $DEST
+    ln -sf $ORIG $DEST
+}
+
 function git()
 {
-  ln -s -i ${DOTFILES_PATH}/git/.gitconfig $HOME/.gitconfig
-  ln -s -i ${DOTFILES_PATH}/git/.gitignore_global $HOME/.gitignore_global
-  ln -s -i ${DOTFILES_PATH}/git/.gitattributes $HOME/.gitattributes
+    lnCommand git/.gitconfig .gitconfig
+    lnCommand git/.gitignore_global .gitignore_global
+    lnCommand git/.gitattributes .gitattributes
 }
 
 function nvim()
 {
-  ln -s -i $DOTFILES_PATH/nvim $HOME/.config/nvim
+  lnCommand nvim .config/nvim
 }
 
 git
--- nvim/plug-config/coc.vim
+++
@@ -1,4 +0,0 @@
-" Explorer
-nmap <space>e :CocCommand explorer<CR>
-nmap <space>f :CocCommand explorer --preset floating<CR>
-autocmd BufEnter * if (winnr("$") == 1 && &filetype == 'coc-explorer') | q | endif
