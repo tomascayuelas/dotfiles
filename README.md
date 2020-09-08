aff169f706f744b6c0b4d68a30efb87ab5bb6992
Tomas Cayuelas <hello@tomascayuelas.com>
2020-09-08 23:33:05 +0200

    Clean and refactor something old stuff

--- README.md
+++ README.md
@@ -2,14 +2,5 @@
 
 This are my ([**@tomascayuelas**](https://twitter.com/tomascayuelas)) dotfiles.
 
-List of software,tools,configs,etc...
-
-- i3wm
-- dunst
-- udiskie
-- pasystray
-- compton
 - git
-
-i3wm    => floating wm
-compton => composite manage
+- nvim
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DOTFILES_PATH="${HOME}/Development/Own/dotfiles"
+export DOTFILES_PATH="${HOME}/Development/dotfiles"
 
 function git()
 {
@@ -9,26 +9,9 @@ function git()
   ln -s -i ${DOTFILES_PATH}/git/.gitattributes $HOME/.gitattributes
 }
 
-function i3wm()
-{
-  rm -irfv ${HOME}/.config/i3
-  ln -s -i ${DOTFILES_PATH}/i3 $HOME/.config/i3
-}
-
 function nvim()
 {
   ln -s -i $DOTFILES_PATH/nvim $HOME/.config/nvim
-}
-
-function xresources()
-{
-  ln -s -i ${DOTFILES_PATH}/X/.Xresources.d $HOME/
-  ln -s -i ${DOTFILES_PATH}/X/.Xresources $HOME/.Xresources
-}
-
-function karabiner()
-{
-  ln -s -i ${DOTFILES_PATH}/mac/karabiner $HOME/.config/karabiner
 }
 
 git
