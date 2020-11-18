b53a8aa684143e4949714306d1b5a5e0756d0cdd
Tomas Cayuelas <hello@tomascayuelas.com>
2020-11-18 13:12:45 +0100

    Remove qtile and refine .gitignore

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-export DOTFILES_PATH="${HOME}/Development/dotfiles"
-
 stow -t $HOME git
 stow -t $HOME nvim
 stow -t $HOME tig
-stow -t $HOME qtile
 stow -t $HOME rofi 
 stow -t $HOME zsh
--- qtile/.config/qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sh /home/tomascayuelas/.screenlayout/two-monitors-one-portrait.sh &
-picom &
-nitrogen --restore &
-dunst &
-pasystray &
-nm-applet &
-blueman-applet &
